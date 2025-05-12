11e329b566a772aeaba93d9f219c4906bcce6eb6
gKonstantin <gkonstantin17@ya.ru>
2025-05-13 01:47:20 +0300

    init projects

---
+++ backend/rs-main/src/main/java/gk17/rsmain/RsMainApplication.java
@@ -0,0 +1,13 @@
+package gk17.rsmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsMainApplication.class, args);
+	}
+
+}
---
+++ backend/rs-main/src/test/java/gk17/rsmain/RsMainApplicationTests.java
@@ -0,0 +1,13 @@
+package gk17.rsmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
