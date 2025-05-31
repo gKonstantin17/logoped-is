7fa98a4106f7a405f942fd4d30c529ef929791ea
gKonstantin <gkonstantin17@ya.ru>
2025-06-01 01:11:05 +0300

    create crud controller, dto remake to record

--- backend/rs-main/src/main/java/gk17/rsmain/controller/UserController.java
+++ backend/rs-main/src/main/java/gk17/rsmain/controller/UserController.java
@@ -1,21 +1,62 @@
 package gk17.rsmain.controller;
 
+import gk17.rsmain.dto.user.UserDto;
+import gk17.rsmain.entity.UserData;
 import gk17.rsmain.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private UserService service;
+    private final UserService service;
     public UserController(UserService service) {
         this.service = service;
     }
+
     @GetMapping("/get")
     public List<UserData> get() {
         return service.get();
     }
+
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody UserDto dto) {
+        if (dto.firstName() == null)
+            return new ResponseEntity<>("Пропущено имя", HttpStatus.BAD_REQUEST);
+        if (dto.secondName() == null)
+            return new ResponseEntity<>("Пропущена фамилия", HttpStatus.BAD_REQUEST);
+        if (dto.email() == null)
+            return new ResponseEntity<>("Пропущен email", HttpStatus.BAD_REQUEST);
+        if (dto.phone() == null)
+            return new ResponseEntity<>("Пропущен телефон", HttpStatus.BAD_REQUEST);
+
+
+        var result = service.create(dto);
+        return result.isSuccess()
+                ? ResponseEntity.ok(result.data())
+                : ResponseEntity.badRequest().body(result.message());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id,@RequestBody UserDto dto) {
+        var result = service.update(id,dto);
+        return result.isSuccess()
+                ? ResponseEntity.ok(result.data())
+                : ResponseEntity.badRequest().body(result.message());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        var result = service.delete(id);
+        return result.isSuccess()
+                ? ResponseEntity.ok(result.data())
+                : ResponseEntity.badRequest().body(result.message());
+    }
+
 }
--- backend/rs-main/src/main/java/gk17/rsmain/service/UserService.java
+++ backend/rs-main/src/main/java/gk17/rsmain/service/UserService.java
@@ -1,5 +1,7 @@
 package gk17.rsmain.service;
 
+import gk17.rsmain.dto.serviceResult.ServiceResult;
+import gk17.rsmain.dto.user.UserDto;
 import gk17.rsmain.entity.UserData;
 import gk17.rsmain.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -8,7 +10,7 @@ import java.util.List;
 
 @Service
 public class UserService {
-    private UserRepository repository;
+    private final UserRepository repository;
     public UserService(UserRepository repository) {
         this.repository = repository;
     }
@@ -16,5 +18,33 @@ public class UserService {
     public List<UserData> get() {
         return repository.findAll();
     }
+    public ServiceResult<UserData> create(UserDto dto) {
+        UserData user  = new UserData();
+        user.setFirstName(dto.firstName());
+        user.setSecondName(dto.secondName());
+        user.setEmail(dto.email());
+        user.setPhone(dto.phone());
+        return ServiceResult.success(repository.save(user));
+    }
+    public ServiceResult<UserData> update(Long id, UserDto dto) {
+        var result = repository.findById(id);
+        if (result.isEmpty())
+            return ServiceResult.error("Пользователь не найден");
 
+        var user = result.get();
+        if (dto.firstName() != null)     user.setFirstName(dto.firstName());
+        if (dto.secondName() != null)    user.setSecondName(dto.secondName());
+        if (dto.email() != null)         user.setEmail(dto.email());
+        if (dto.phone() != null)         user.setPhone(dto.phone());
+        return ServiceResult.success(repository.save(user));
+    }
+    public ServiceResult<UserData> delete(Long id) {
+        var result = repository.findById(id);
+        if (result.isEmpty())
+            return ServiceResult.error("Пользователь не найден");
+
+        var user = result.get();
+        repository.deleteById(id);
+        return ServiceResult.success(user);
+    }
 }
