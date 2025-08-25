44888de2edca3de1f326d104b064b29ef072b134
gKonstantin <gkonstantin17@ya.ru>
2025-08-25 15:52:40 +0300

    create ms notification

---
+++ backend-ms/ms-notificaton/src/main/java/logopedis/msnotificaton/MsNotificatonApplication.java
@@ -0,0 +1,13 @@
+package logopedis.msnotificaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsNotificatonApplication.class, args);
+    }
+
+}
---
+++ backend-ms/ms-notificaton/src/test/java/logopedis/msnotificaton/MsNotificatonApplicationTests.java
@@ -0,0 +1,13 @@
+package logopedis.msnotificaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
