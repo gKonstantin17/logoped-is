1d4ff9c5c849db8ba092c8e80f76cf8d28108e06
gKonstantin <gkonstantin17@ya.ru>
2025-09-02 11:58:29 +0300

    add ms url in FE

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@ export const environment = {
   production: false,
   //RESOURSE_URL: 'http://localhost:8280',
   RESOURSE_URL: 'http://localhost:8765/rs-main',
+  MS_NOTIFICATION_URL: 'http://localhost:8765/ms-notification',
   KC_CLIENT_ID:'logoped-client',
   KC_URI:'http://localhost:8180/realms/logoped-realm/protocol/openid-connect',
-  // RESOURCE_URI : "http://localhost:8765/rs",
   BFF_URI:"http://localhost:8380",
   REDIRECT_URL: 'http://localhost:4200/login'
 };
